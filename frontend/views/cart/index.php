a094371d3a88121bcab749de7827e8b242d6fa05
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-12-18 10:16:33 +0400

    Display price with currency in cart page

--- frontend/views/cart/index.php
+++ frontend/views/cart/index.php
@@ -35,11 +35,11 @@
                              style="width: 50px;"
                              alt="<?php echo $item['name'] ?>">
                     </td>
-                    <td><?php echo $item['price'] ?></td>
+                    <td><?php echo Yii::$app->formatter->asCurrency($item['price']) ?></td>
                     <td>
                         <input type="number" min="1" class="form-control item-quantity" style="width: 60px" value="<?php echo $item['quantity'] ?>">
                     </td>
-                    <td><?php echo $item['total_price'] ?></td>
+                    <td><?php echo Yii::$app->formatter->asCurrency($item['total_price']) ?></td>
                     <td>
                         <?php echo \yii\helpers\Html::a('Delete', ['/cart/delete', 'id' => $item['id']], [
                             'class' => 'btn btn-outline-danger btn-sm',
