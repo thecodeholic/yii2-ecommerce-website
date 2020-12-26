8fa7b05e660ec930caf6a90d0dadf14a53bc2d0b
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-12-26 08:01:58 +0400

    Implement search by fullname in order list

--- backend/models/search/OrderSearch.php
+++ backend/models/search/OrderSearch.php
@@ -11,6 +11,7 @@ use common\models\Order;
  */
 class OrderSearch extends Order
 {
+    public $fullname;
     /**
      * {@inheritdoc}
      */
@@ -19,8 +20,17 @@ class OrderSearch extends Order
         return [
             [['id', 'status', 'created_at', 'created_by'], 'integer'],
             [['total_price'], 'number'],
-            [['firstname', 'lastname', 'email', 'transaction_id', 'paypal_order_id'], 'safe'],
+            [['firstname', 'lastname', 'fullname', 'email', 'transaction_id', 'paypal_order_id'], 'safe'],
         ];
+    }
+
+    public function fields()
+    {
+        return array_merge(parent::fields(), [
+            'fullname' => function () {
+                return $this->firstname . ' ' . $this->lastname;
+            }
+        ]);
     }
 
     /**
@@ -48,13 +58,21 @@ class OrderSearch extends Order
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
         ]);
+        $dataProvider->sort->attributes['fullname'] = [
+            'label' => 'Full Name',
+            'asc' => ['firstname' => SORT_ASC, 'lastname' => SORT_ASC],
+            'desc' => ['firstname' => SORT_DESC, 'lastname' => SORT_DESC],
+        ];
 
         $this->load($params);
 
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
+//             $query->where('0=1');
             return $dataProvider;
+        }
+        if ($this->fullname) {
+            $query->andWhere("CONCAT(firstname, ' ', lastname) LIKE :fullname", ['fullname' => "%{$this->fullname}%"]);
         }
 
         // grid filtering conditions
