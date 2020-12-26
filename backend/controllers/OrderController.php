6392bfb8b5c67dcefeff99a709d1c187d4eb4192
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2020-12-26 08:01:36 +0400

    Remove update action from order controller

--- backend/controllers/OrderController.php
+++ backend/controllers/OrderController.php
@@ -76,26 +76,6 @@ class OrderController extends Controller
     }
 
     /**
-     * Updates an existing Order model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
      * Deletes an existing Order model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      * @param integer $id
